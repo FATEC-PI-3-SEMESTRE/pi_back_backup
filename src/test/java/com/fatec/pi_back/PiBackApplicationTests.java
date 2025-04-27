e4d2e624ca8184ad669125fa5af9062d40cc60d4
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-04-27 10:13:50 -0300

    chore: iniciar projeto Spring Boot (v0.0.0-beta)

---
+++ src/test/java/com/fatec/pi_back/PiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.pi_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

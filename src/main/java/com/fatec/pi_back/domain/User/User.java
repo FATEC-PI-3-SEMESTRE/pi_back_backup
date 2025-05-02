4b2823957cd336ab5e33b01a4a3a9350373fb9c5
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-02 15:04:16 -0300

    :sparkles: feat: User domain

---
+++ src/main/java/com/fatec/pi_back/domain/User/User.java
@@ -0,0 +1,51 @@
+package com.fatec.pi_back.domain.User;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String email;
+    private String password;
+    private Boolean access;
+    
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", updatable = false)
+    private User createdBy;
+
+    @ManyToOne
+    @JoinColumn(name = "updated_by")
+    private User updatedBy;    
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+}
---
+++ src/main/java/com/fatec/pi_back/domain/User/UserDTO.java
@@ -0,0 +1,5 @@
+package com.fatec.pi_back.domain.User;
+
+public record UserDTO (String email, String password, Boolean access) {
+    
+}

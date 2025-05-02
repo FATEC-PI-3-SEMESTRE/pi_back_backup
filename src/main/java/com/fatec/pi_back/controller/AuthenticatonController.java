c5f3ead25c47490ca2bc27dc027dcc0c1f39dc6b
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2025-05-02 20:21:40 -0300

    :sparkles: feat: authentication service and controller + security

---
+++ src/main/java/com/fatec/pi_back/controller/AuthenticatonController.java
@@ -0,0 +1,64 @@
+package com.fatec.pi_back.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fatec.pi_back.domain.Login.LoginDTO;
+import com.fatec.pi_back.domain.Login.LoginResponseDTO;
+import com.fatec.pi_back.domain.User.User;
+import com.fatec.pi_back.domain.User.UserDTO;
+import com.fatec.pi_back.repository.UserRepository;
+import com.fatec.pi_back.service.TokenService;
+
+import jakarta.validation.Valid;
+
+import javax.naming.AuthenticationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+    @RestController
+    @RequestMapping("auth")
+    public class AuthenticatonController {
+        @Autowired
+        private UserRepository repository;
+
+        @Autowired
+        private TokenService tokenService;
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @PostMapping("/login")
+        public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid LoginDTO data) {
+            var userPassword = new UsernamePasswordAuthenticationToken(data.email(), data.password());
+            System.out.println("=================================================================");
+            System.out.println(userPassword);
+            System.out.println("=================================================================");
+            
+            var auth = this.authenticationManager.authenticate(userPassword);
+            System.out.println("=================================================================");
+            System.out.println(auth);
+            System.out.println("=================================================================");
+            var token = tokenService.generateToken((User) auth.getPrincipal());
+                
+            return ResponseEntity.ok(new LoginResponseDTO(token));
+
+        }
+
+        @PostMapping("/register")
+        public ResponseEntity<String> register(@RequestBody @Valid UserDTO data) {
+            if(this.repository.findByEmail(data.email()) != null) return ResponseEntity.badRequest().build();
+            String encriptedPassword = new BCryptPasswordEncoder().encode(data.password());
+            User newUser = new User(data.email(), encriptedPassword, data.access());
+
+            this.repository.save(newUser);
+            return ResponseEntity.ok().build();
+
+        }
+    }
---
+++ src/main/java/com/fatec/pi_back/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package com.fatec.pi_back.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.fatec.pi_back.repository.UserRepository;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
+    }
+}

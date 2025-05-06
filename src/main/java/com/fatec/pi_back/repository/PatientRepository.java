0331de94c4f557a5427c80d78cb6b2fb85d42c9c
Zamba-Vitor <zambaestuda@gmail.com>
2025-05-06 13:18:21 -0300

    feat: create the Repository for Patient

---
+++ src/main/java/com/fatec/pi_back/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package main.java.com.fatec.pi_back.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PatientRepository extends JpaRepository <Patient,Integer>{
+    
+}
+   
+
